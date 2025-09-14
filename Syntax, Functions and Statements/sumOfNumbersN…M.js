74909f2d9c5e1dedbbef06e87809dfab73ea4e48
Anina1999 <anina_1999@abv.bg>
2025-09-14 21:22:02 +0300

    Add sumOfNumbersN...M.js

---
+++ Syntax, Functions and Statements/sumOfNumbersN…M.js
@@ -0,0 +1,13 @@
+function printTheSumOfAllNumbers(n, m) {
+    let firstNum = Number(n);
+    let secondNum = Number(m);
+    let sum = 0;
+    
+    for (let i = firstNum; i <= secondNum; i++) {
+        sum += i;
+    }
+    
+    console.log(sum);
+}
+
+//printTheSumOfAllNumbers('-8', '20');
