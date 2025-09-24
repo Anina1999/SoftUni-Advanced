f72eefae7b3443df9b4cd3643eb4132f7d693c3e
Anina1999 <anina_1999@abv.bg>
2025-09-24 12:36:28 +0300

    Add cityRecord.js

---
+++ Objects and Composition/cityRecord.js
@@ -0,0 +1,16 @@
+//solve 1
+function recordCity(cityName, numberOfPopulation, treasureNum) {
+    let cityInfo = {};
+
+    cityInfo.name = cityName;
+    cityInfo.population = numberOfPopulation;
+    cityInfo.treasury = treasureNum;
+
+    return cityInfo;
+}
+
+/*console.log(recordCity('Tortuga',
+
+7000,
+
+15000));*/
