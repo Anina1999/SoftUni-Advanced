df2b5b4959795524ab382f8d21ce9b6dba7e5537
Anina1999 <anina_1999@abv.bg>
2025-09-30 22:57:55 +0300

    Add showMore.js

---
+++ DOM Introduction/DOM Introduction/04.Show More/showMore.js
@@ -0,0 +1,7 @@
+function showText() {
+    const button = document.getElementById('more');
+    const text = document.getElementById('text');
+
+    button.style.display = 'none';
+    text.style.display = 'inline'
+}
