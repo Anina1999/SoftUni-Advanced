b723d62f95e8d37d6ca02b511689a1741594fcf4
Anina Angelova <anina_1999@abv.bg>
2025-10-12 12:44:32 +0300

    Add app.js

---
+++ DOM Introduction/Exercise DOM Manipulations and Events/05.JS-Advanced-DOM-Events-Exercise-Resources/03. Locked-Profile/app.js
@@ -0,0 +1,24 @@
+function lockedProfile() {
+    const btnsRef = document.querySelectorAll('button');
+    Array.from(btnsRef).forEach(btn => btn.addEventListener('click', onClick));
+
+    function onClick(e) {
+        const btn = e.target;
+        const card = btn.parentElement;
+        const radioBtn = Array.from(card.querySelectorAll("input[type='radio']")).filter(x => x.checked)[0];
+        const profileState = radioBtn.value;
+        const moreInfoDiv = card.querySelector('div');
+
+        if (profileState === 'lock') {
+            return;
+        }
+
+        if (btn.textContent === 'Show more') {
+            moreInfoDiv.style.display = 'block';
+            btn.textContent = 'Hide it';
+        } else {
+            moreInfoDiv.style.display = 'none';
+            btn.textContent = 'Show more';
+        }
+    }
+}
