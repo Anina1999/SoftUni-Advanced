7a1937f6334e5e5acf7bf896eab64a3ef3bb864c
Anina Angelova <anina_1999@abv.bg>
2025-10-15 21:31:38 +0300

    Add add.js

---
+++ Advanced Functions/add.js
@@ -0,0 +1,13 @@
+function add(n) {
+
+    return addAnotherValue;
+
+    function addAnotherValue(m) {
+        let num  = n + m;
+        return num;
+    }
+}
+
+// let add5 = add(5);
+// console.log(add5(2));
+// console.log(add5(3));
