98209d1ed729fffc44efd911276f1fffbf05acad
Anina Angelova <anina_1999@abv.bg>
2025-10-16 11:23:02 +0300

    Add commandProcessor.js

---
+++ Advanced Functions/commandProcessor.js
@@ -0,0 +1,33 @@
+function commandProcessor() {
+    let str = '';
+
+    return {
+        append,
+        removeStart,
+        removeEnd,
+        print
+    };
+
+    function append(value) {
+        str += value;
+    }
+
+    function removeStart(n) {
+        str = str.slice(n);
+    }
+
+    function removeEnd(n) {
+        str = str.slice(0, -n);
+    }
+
+    function print() {
+        console.log(str);
+    }
+}
+
+// const processor = commandProcessor();
+// processor.append('hello');
+// processor.append('again');
+// processor.removeStart(3);
+// processor.removeEnd(4);
+// processor.print();
