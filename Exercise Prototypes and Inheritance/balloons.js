f4885ed587df29aa8715d10a5080d341bdadb7ed
Anina Angelova <anina_1999@abv.bg>
2025-10-29 20:14:47 +0200

    Add balloons.js

---
+++ Exercise Prototypes and Inheritance/balloons.js
@@ -0,0 +1,55 @@
+function balloons() {
+    class Balloon {
+        color;
+        gasWeight;
+        constructor(color, gasWeight) {
+            this.color = color;
+            this.gasWeight = gasWeight;
+        }
+    }
+
+    class PartyBalloon extends Balloon {
+        _ribbon;
+        constructor(color, gasWeight, ribbonColor, ribbonLength) {
+            super(color, gasWeight);
+            this._ribbon = {
+                color: ribbonColor,
+                length: ribbonLength
+            }
+        }
+
+        get ribbon() {
+            return this._ribbon;
+        }
+    }
+
+    class BirthdayBalloon extends PartyBalloon {
+        _text;
+        constructor(color, gasWeight, ribbonColor, ribbonLength, text) {
+            super(color, gasWeight, ribbonColor, ribbonLength);
+            this._text = text;
+        }
+
+        get text() {
+            return this._text;
+        }
+    }
+
+    return {
+        Balloon, PartyBalloon, BirthdayBalloon
+    }
+}
+
+let classes = balloons();
+let testBalloon = new classes.Balloon(
+"yellow", 20.5);
+
+let testPartyBalloon = new
+classes.PartyBalloon("yellow", 20.5,
+"red", 10.25);
+
+let ribbon = testPartyBalloon.ribbon;
+
+console.log(testBalloon);
+console.log(testPartyBalloon);
+console.log(ribbon);
